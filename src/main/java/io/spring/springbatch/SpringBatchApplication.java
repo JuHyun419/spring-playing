1e5a398e03197c9c7e15e4fe8c08f4b4e753cac0
JuHyun419 <zzang9ha@naver.com>
2022-10-29 16:24:22 +0900

    feat: @EnableBatchProcessing 추가

--- src/main/java/io/spring/springbatch/SpringBatchApplication.java
+++ src/main/java/io/spring/springbatch/SpringBatchApplication.java
@@ -1,8 +1,10 @@
 package io.spring.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
 
