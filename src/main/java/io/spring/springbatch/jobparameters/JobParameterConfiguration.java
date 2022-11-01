7f752c934788a61eb7bb3b8ee000729dcd7460ae
JuHyun419 <zzang9ha@naver.com>
2022-11-01 21:39:02 +0900

    feat: add Spring Batch Job JobParameters code

---
+++ src/main/java/io/spring/springbatch/jobparameters/JobParameterConfiguration.java
@@ -0,0 +1,51 @@
+package io.spring.springbatch.jobparameters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobParameterConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("JobParameters")
+                .start(step1(null))
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("JobParametersStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("=======================");
+                    System.out.println("requestDate: " + requestDate);
+                    System.out.println("=======================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("JobParametersStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
---
+++ src/main/java/io/spring/springbatch/jobparameters/JobParameterTest.java
@@ -0,0 +1,33 @@
+package io.spring.springbatch.jobparameters;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JobParameterTest implements ApplicationRunner {
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder().addString("name", "JuHyuns")
+                .addLong("seq", 2L)
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+    }
+}
