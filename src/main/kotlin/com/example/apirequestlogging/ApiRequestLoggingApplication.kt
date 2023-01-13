86da252b6c402089aedfd9bc69ffdcddde8b3f23
juhyun <zzang9ha@naver.com>
2023-01-14 00:02:39 +0900

    feat: add @EnableAsync

--- src/main/kotlin/com/example/apirequestlogging/ApiRequestLoggingApplication.kt
+++ src/main/kotlin/com/example/apirequestlogging/ApiRequestLoggingApplication.kt
@@ -2,7 +2,9 @@ package com.example.apirequestlogging
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class ApiRequestLoggingApplication
 
