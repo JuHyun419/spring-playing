908a2a17687db96e1ed1534627c5d4a632b69220
juhyun <zzang9ha@naver.com>
2023-01-21 20:49:15 +0900

    initial commit

---
+++ spring-multi-datasource-jpa/src/main/java/com/example/springmultidatasourcejpa/SpringMultiDatasourceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmultidatasourcejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiDatasourceJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiDatasourceJpaApplication.class, args);
+    }
+
+}
