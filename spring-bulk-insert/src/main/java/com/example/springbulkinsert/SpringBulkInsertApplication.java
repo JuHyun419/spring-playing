72304f80a76b770d7a6b898838b23d0308469a8c
juhyun <zzang9ha@naver.com>
2023-01-31 00:41:59 +0900

    initial commit

---
+++ spring-bulk-insert/src/main/java/com/example/springbulkinsert/SpringBulkInsertApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbulkinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBulkInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBulkInsertApplication.class, args);
+	}
+
+}
