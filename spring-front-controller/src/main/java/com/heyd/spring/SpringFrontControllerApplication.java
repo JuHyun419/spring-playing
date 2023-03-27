613307db469f9f502820786206fafafdb83f5b21
juhyun <zzang9ha@naver.com>
2023-03-28 00:41:53 +0900

    [spring-front-controller] initial commit

---
+++ spring-front-controller/src/main/java/com/heyd/spring/SpringFrontControllerApplication.java
@@ -0,0 +1,13 @@
+package com.heyd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrontControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrontControllerApplication.class, args);
+	}
+
+}
---
+++ spring-front-controller/src/test/java/com/heyd/spring/SpringFrontControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrontControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
